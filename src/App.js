47a796047772de44205cbbdbf91b4487d271965e
Cullen Botha <culbot@gmail.com>
2021-07-18 20:36:33 +0200

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -2,30 +2,60 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
+// test data
+const _cookies = [
+  { domain: 'youtube.com', name: 'cookie 1', value: '13423524542935023894509234' },
+  { domain: 'google.com', name: 'cookie 2', value: '13423524542935023894509234' },
+  { domain: 'bing.com', name: 'cookie 3', value: '13423524542935023894509234' },
+  { domain: 'yandex.com', name: 'cookie 4', value: '13423524542935023894509234' },
+]
+
 const App = () => {
   const [cookies, setCookies] = useState([])
   const [domain, setDomain] = useState()
 
-  useEffect(() => {
+  const fetchCookies = () => {
+    if (domain) {
+      chrome.cookies.getAll({ domain: domain }).then((result) => {
+        setCookies(result)
+      })
+    }
+  }
+
+  const fetchDomain = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var url = new URL(tabs[0].url).hostname
-      let domain = url.replace('www.', '.')
+      const url = new URL(tabs[0].url).hostname
+      const domain = baseDomain(url)
       setDomain(domain)
     })
-  }, [])
+  }
 
-  useEffect(() => {
-    chrome.cookies.getAll({ domain: domain }).then((result) => {
-      setCookies(result)
-    })
-  }, [domain])
+  useEffect(fetchDomain, [])
+
+  useEffect(fetchCookies, [domain])
+
+  const baseDomain = (url) => {
+    const ar = url.split('.')
+    return ar.slice(-2).join('.')
+  }
+
+  const togglePassword = (el) => {
+    const type = el.target.parentElement.firstElementChild.type
+    let newType = type == 'password' ? 'text' : 'password'
+    el.target.parentElement.firstElementChild.type = newType
+  }
 
   const ListCookies = () => {
-    return cookies.map((cookie) => (
+    return cookies.map((cookie, index) => (
       <tr>
         <td>{cookie.domain}</td>
         <td>{cookie.name}</td>
-        <td>{cookie.value}</td>
+        <td>
+          <span>
+            <input type="password" value={cookie.value} />
+            <a onClick={togglePassword}> X</a>
+          </span>
+        </td>
       </tr>
     ))
   }
