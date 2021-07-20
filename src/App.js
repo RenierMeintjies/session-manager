2241bdb1d51e26368199b1facfc80d71bca6a34a
Renier Meintjies <reniermeintjies@gmail.com>
2021-07-20 10:06:07 +0200

    extract togglePassword function to CookieValue component + add component to ListCookies

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 /*global chrome*/
 import './App.css'
-// import _cookies from './seed.js'
 import { useEffect, useState } from 'react'
+import EditIcon from './components/EditCookie'
+import { CookieValue } from './components/CookieValue'
+
+import { _cookies } from './seed'
 
 const App = () => {
-  const [cookies, setCookies] = useState([])
-  const [domain, setDomain] = useState()
+  const [cookies, setCookies] = useState(_cookies)
+  const [domain, setDomain] = useState('')
 
   const fetchCookies = () => {
     if (domain) {
-      chrome.cookies.getAll({ domain: domain }).then((result) => {
-        setCookies(result)
-      })
+      // chrome.cookies.getAll({ domain: domain }).then((result) => {
+      //   setCookies(result)
+      // })
     }
   }
 
   const fetchDomain = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const url = new URL(tabs[0].url).hostname
-      const domain = baseDomain(url)
-      setDomain(domain)
-    })
+    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   const url = new URL(tabs[0].url).hostname
+    //   const domain = baseDomain(url)
+    //   setDomain(domain)
+    // })
   }
 
-  useEffect(fetchDomain, [])
+  // useEffect(fetchDomain, [])
 
-  useEffect(fetchCookies, [domain])
+  // useEffect(fetchCookies, [domain])
 
   const baseDomain = (url) => {
     const ar = url.split('.')
     return ar.slice(-2).join('.')
   }
 
-  const togglePassword = (el) => {
-    const type = el.target.parentElement.firstElementChild.type
-    let newType = type === 'password' ? 'text' : 'password'
-    el.target.parentElement.firstElementChild.type = newType
-  }
-
   const ListCookies = () => {
     return cookies.map((cookie, index) => (
-      <tr>
+      <tr key={`cookies-${cookie.domain}-${index}`}>
         <td>{cookie.domain}</td>
         <td>{cookie.name}</td>
         <td>
           <span style={{ display: 'flex', flexDirection: 'row' }}>
-            <input type="password" value={cookie.value} />
-            <button style={{ marginLeft: '3px' }} onClick={togglePassword}>
-              X
-            </button>
+            <CookieValue value={cookie.value} />
+            <EditIcon />
           </span>
         </td>
       </tr>
@@ -58,14 +53,14 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>Domain: {domain}</h4>
+        <h4>Domain: </h4>
         <table>
           <tr>
             <th>Domain</th>
             <th>Key</th>
             <th>Value</th>
           </tr>
-          <ListCookies></ListCookies>
+          <ListCookies />
         </table>
       </header>
     </div>
