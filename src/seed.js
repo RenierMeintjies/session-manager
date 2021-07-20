a1abdeaf2c592b5d49507c508cf51ee5bbb96076
Renier Meintjies <reniermeintjies@gmail.com>
2021-07-20 09:49:53 +0200

    add seed.js

---
+++ src/seed.js
@@ -0,0 +1,19 @@
+// test data
+export const _cookies = [
+  {
+    domain: 'youtube.com',
+    name: 'cookie 1',
+    value: '13423524542935023894509234',
+  },
+  {
+    domain: 'google.com',
+    name: 'cookie 2',
+    value: '13423524542935023894509234',
+  },
+  { domain: 'bing.com', name: 'cookie 3', value: '13423524542935023894509234' },
+  {
+    domain: 'yandex.com',
+    name: 'cookie 4',
+    value: '13423524542935023894509234',
+  },
+]
