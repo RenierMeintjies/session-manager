3a8487045d4543b5457888d609e54c6ac2869b27
Cullen Botha <culbot@gmail.com>
2021-07-23 17:52:30 +0200

    edit cookie form WIP

--- src/components/CookieList/CookieRow.tsx
+++ src/components/CookieList/CookieRow.tsx
@@ -5,15 +5,22 @@ import {
   VisibilityOff as VisibilityOffIcon,
   Delete as DeleteIcon,
   FileCopy as CopyIcon,
+  Edit as EditIcon,
 } from '@material-ui/icons'
+import Form from './Form'
 
 const CookieRow = ({ cookie }: any) => {
   const [visible, setVisible] = useState(false)
+  const [editMode, setEditMode] = useState(false)
 
   const handleCookieDelete = () => {
     if (window.confirm(`Are you sure you want to delete the '${cookie.name}' cookie for '${cookie.domain}'?`)) {
       chromeApi.deleteCookie(cookie)
     }
+  }
+
+  const handleEditCookie = () => {
+    setEditMode(!editMode)
   }
 
   const handleValueCopy = () => {
@@ -27,18 +34,22 @@ const CookieRow = ({ cookie }: any) => {
   const VisibilityToggle = visible ? VisibilityIcon : VisibilityOffIcon
 
   return (
-    <tr>
-      <td>{cookie.domain}</td>
-      <td>{cookie.name}</td>
-      <td>
-        <input id="value-input" type={visible ? 'text' : 'password'} value={cookie.value} />
-      </td>
-      <td style={{ display: 'flex', flexDirection: 'row' }}>
-        <VisibilityToggle onClick={handleValueToggle} />
-        <CopyIcon onClick={handleValueCopy} />
-        <DeleteIcon onClick={handleCookieDelete} />
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td>{cookie.domain}</td>
+        <td>{cookie.name}</td>
+        <td>
+          <input id="value-input" type={visible ? 'text' : 'password'} value={cookie.value} />
+        </td>
+        <td style={{ display: 'flex', flexDirection: 'row' }}>
+          <VisibilityToggle onClick={handleValueToggle} />
+          <EditIcon onClick={handleEditCookie}></EditIcon>
+          <CopyIcon onClick={handleValueCopy} />
+          <DeleteIcon onClick={handleCookieDelete} />
+        </td>
+      </tr>
+      {editMode && <Form cookie={cookie} />}
+    </>
   )
 }
 
