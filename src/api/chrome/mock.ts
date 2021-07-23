f5a3ae0a554ed073c0062cbc39d11e66bce40eed
Cullen Botha <culbot@gmail.com>
2021-07-23 17:51:41 +0200

    add json-server, mock chrome cookie api for dev

--- src/api/chrome/mock.ts
+++ src/api/chrome/mock.ts
@@ -1,37 +1,40 @@
-var _COOKIES = [
-  { domain: 'github.com', name: 'cookie 1', value: '134235421352312374' },
-  { domain: 'github.com', name: 'cookie 2', value: '32471342273355478398' },
-  { domain: 'github.com', name: 'cookie 3', value: '2114247421342352234532' },
-  { domain: 'github.com', name: 'cookie 4', value: '3246237234134235536345' },
-  { domain: 'stackoverflow.com', name: 'cookie 5', value: '3425129350223453863' },
-  { domain: 'stackoverflow.com', name: 'cookie 6', value: '1564564563212343547' },
-  { domain: 'stackoverflow.com', name: 'cookie 7', value: '2345432543262364867' },
-  { domain: 'stackoverflow.com', name: 'cookie 8', value: '6234623462346324811' },
-  { domain: 'stackoverflow.com', name: 'cookie 9', value: '6342634263246342623' },
-]
+const jsonCookiesUrl = 'http://localhost:3001/cookies'
 
 const deleteCookie = (cookie: any) => {
-  return new Promise((resolve) => {
-    resolve(true)
-  })
+  httpRequest('delete', jsonCookiesUrl + `/${cookie.id}`)
+  dispatchCookiesChangedEvent()
 }
 
 const fetchCookies = async (domain: any) => {
-  return new Promise((resolve) => {
-    resolve(_COOKIES)
-  })
+  return (await httpRequest('get', jsonCookiesUrl)).json()
 }
 
 const fetchDomain = async () => {
   return new Promise((resolve) => {
-    resolve('github.com')
+    resolve('domain-1.com')
   })
 }
 
 const onCookieChange = (onChange: Function) => {
-  return new Promise((resolve) => {
-    resolve(onChange())
+  window.addEventListener('onCookieChange', () => {
+    onChange()
   })
+}
+
+// private
+
+const dispatchCookiesChangedEvent = () => {
+  window.dispatchEvent(new CustomEvent('onCookieChange', {}))
+}
+
+const httpRequest = async (method: string, url: string) => {
+  const response = await fetch(url, {
+    method: method.toUpperCase(),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+  return response
 }
 
 export default { deleteCookie, fetchCookies, fetchDomain, onCookieChange }
