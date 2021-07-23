c62713c7f6fd274de1fa95b0c1c2640f5161a241
Renier Meintjies <reniermeintjies@gmail.com>
2021-07-23 15:55:29 +0200

    format change

--- src/api/chrome/mock.ts
+++ src/api/chrome/mock.ts
@@ -1,37 +1,37 @@
 var _COOKIES = [
-  { domain: 'github.com', name: 'cookie 1', value: '134235421352312374' },
-  { domain: 'github.com', name: 'cookie 2', value: '32471342273355478398' },
-  { domain: 'github.com', name: 'cookie 3', value: '2114247421342352234532' },
-  { domain: 'github.com', name: 'cookie 4', value: '3246237234134235536345' },
-  { domain: 'stackoverflow.com', name: 'cookie 5', value: '3425129350223453863' },
-  { domain: 'stackoverflow.com', name: 'cookie 6', value: '1564564563212343547' },
-  { domain: 'stackoverflow.com', name: 'cookie 7', value: '2345432543262364867' },
-  { domain: 'stackoverflow.com', name: 'cookie 8', value: '6234623462346324811' },
-  { domain: 'stackoverflow.com', name: 'cookie 9', value: '6342634263246342623' },
+	{ domain: 'github.com', name: 'cookie 1', value: '134235421352312374' },
+	{ domain: 'github.com', name: 'cookie 2', value: '32471342273355478398' },
+	{ domain: 'github.com', name: 'cookie 3', value: '2114247421342352234532' },
+	{ domain: 'github.com', name: 'cookie 4', value: '3246237234134235536345' },
+	{ domain: 'stackoverflow.com', name: 'cookie 5', value: '3425129350223453863' },
+	{ domain: 'stackoverflow.com', name: 'cookie 6', value: '1564564563212343547' },
+	{ domain: 'stackoverflow.com', name: 'cookie 7', value: '2345432543262364867' },
+	{ domain: 'stackoverflow.com', name: 'cookie 8', value: '6234623462346324811' },
+	{ domain: 'stackoverflow.com', name: 'cookie 9', value: '6342634263246342623' },
 ]
 
 const deleteCookie = () => {
-  return new Promise((resolve) => {
-    resolve(true)
-  })
+	return new Promise(resolve => {
+		resolve(true)
+	})
 }
 
 const fetchCookies = async () => {
-  return new Promise((resolve) => {
-    resolve(_COOKIES)
-  })
+	return new Promise(resolve => {
+		resolve(_COOKIES)
+	})
 }
 
 const fetchDomain = async () => {
-  return new Promise((resolve) => {
-    resolve('github.com')
-  })
+	return new Promise(resolve => {
+		resolve('github.com')
+	})
 }
 
 const onCookieChange = (onChange: Function) => {
-  return new Promise((resolve) => {
-    resolve(onChange())
-  })
+	return new Promise(resolve => {
+		resolve(onChange())
+	})
 }
 
 export { deleteCookie, fetchCookies, fetchDomain, onCookieChange }
--- src/components/CookieList/index.tsx
+++ src/components/CookieList/index.tsx
@@ -5,33 +5,33 @@ import { useCookies } from './hooks'
 import chromeApi from '../../api/chrome'
 
 const CookieList = () => {
-  const cookies = useCookies()
+	const cookies = useCookies()
 
-  const handleDeleteAll = () => {
-    if (window.confirm(`Are you sure you want to delete all cookies?`)) {
-      cookies.forEach((cookie: any) => {
-        chromeApi.deleteCookie(cookie)
-      })
-    }
-  }
+	const handleDeleteAll = () => {
+		if (window.confirm(`Are you sure you want to delete all cookies?`)) {
+			cookies.forEach((cookie: any) => {
+				chromeApi.deleteCookie(cookie)
+			})
+		}
+	}
 
-  return (
-    <table>
-      <tr>
-        <th>Domain</th>
-        <th>Key</th>
-        <th>Value</th>
-        <th>{cookies.length > 0 && <DeleteIcon onClick={handleDeleteAll} />}</th>
-      </tr>
-      {cookies.length > 0 ? (
-        cookies.map((cookie: any, index: any) => (
-          <CookieRow key={`cookie-${cookie.domain}-${index}`} cookie={cookie} index={index} />
-        ))
-      ) : (
-        <h4>No cookies for you :c</h4>
-      )}
-    </table>
-  )
+	return (
+		<table>
+			<tr>
+				<th>Domain</th>
+				<th>Key</th>
+				<th>Value</th>
+				<th>{cookies.length > 0 && <DeleteIcon onClick={handleDeleteAll} />}</th>
+			</tr>
+			{cookies.length > 0 ? (
+				cookies.map((cookie: any, index: any) => (
+					<CookieRow key={`cookie-${cookie.domain}-${index}`} cookie={cookie} index={index} />
+				))
+			) : (
+				<h4>No cookies for you :c</h4>
+			)}
+		</table>
+	)
 }
 
 export default CookieList
