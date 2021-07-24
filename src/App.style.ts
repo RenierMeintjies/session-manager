94ca14002c6762b6763d05379eea43ebe69275bb
Renier Meintjies <reniermeintjies@gmail.com>
2021-07-24 22:29:43 +0200

    add margin: auto

--- src/App.style.ts
+++ src/App.style.ts
@@ -6,6 +6,7 @@ export const AppContainer = styledComponents.div`
   min-height: 100vh;
   flex-direction: column;
   max-width: fit-content;
+  margin: auto;
   background-color: ${theme.palette.bg};
   color:  ${theme.palette.text};
 `
