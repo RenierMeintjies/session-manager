365295afe4386c8c169bff252673b3abc72ac9c3
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2021-08-10 16:59:39 +0200

    extract DynamicInput into own component file

--- src/components/CookieList/Form.tsx
+++ src/components/CookieList/Form.tsx
@@ -1,53 +1,7 @@
 import { NoCookies } from './index.style'
-
-interface inputData {
-  _key: string
-  value: number | string | boolean | object
-}
+import DynamicInput from './DynamicInput'
 
 const Form = ({ data }: any) => {
-  const Input = ({ _key, value }: inputData) => {
-    return (
-      <span>
-        <h4>{_key}:</h4>
-        <input defaultValue={value.toString() || ''} />
-      </span>
-    )
-  }
-
-  const TextArea = ({ _key, value }: inputData) => {
-    return (
-      <span>
-        <h4>{_key}:</h4>
-        <textarea defaultValue={value.toString() || ''} />
-      </span>
-    )
-  }
-
-  const Checkbox = ({ _key, value }: inputData) => {
-    return (
-      <span>
-        <h4>{_key}:</h4>
-        <input type="checkbox" defaultValue={value.toString() || ''} />
-      </span>
-    )
-  }
-
-  const DynamicInput = ({ _key, value }: inputData) => {
-    if (typeof value === 'string') {
-      return value.length > 30 ? <TextArea _key={_key} value={value} /> : <Input _key={_key} value={value} />
-    } else if (typeof value == 'boolean') {
-      return <Checkbox _key={_key} value={value} />
-    } else {
-      return (
-        <span>
-          <h4>{_key}:</h4>
-          <input defaultValue={value.toString() || ''} />
-        </span>
-      )
-    }
-  }
-
   return (
     <>
       {data ? (
