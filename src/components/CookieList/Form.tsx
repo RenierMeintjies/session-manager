6a8b1a38f5872b535e95f377d3c4fef9f39383be
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2021-08-10 16:23:19 +0200

    change DynamicInput component

--- src/components/CookieList/Form.tsx
+++ src/components/CookieList/Form.tsx
@@ -2,7 +2,7 @@ import { NoCookies } from './index.style'
 
 interface inputData {
   _key: string
-  value: number | string | boolean
+  value: number | string | boolean | object
 }
 
 const Form = ({ data }: any) => {
@@ -14,6 +14,7 @@ const Form = ({ data }: any) => {
       </span>
     )
   }
+
   const TextArea = ({ _key, value }: inputData) => {
     return (
       <span>
@@ -22,14 +23,7 @@ const Form = ({ data }: any) => {
       </span>
     )
   }
-  const Datepicker = ({ _key, value }: inputData) => {
-    return (
-      <span>
-        <h4>{_key}:</h4>
-        <input type="date" defaultValue={value.toString() || ''} />
-      </span>
-    )
-  }
+
   const Checkbox = ({ _key, value }: inputData) => {
     return (
       <span>
@@ -42,6 +36,8 @@ const Form = ({ data }: any) => {
   const DynamicInput = ({ _key, value }: inputData) => {
     if (typeof value === 'string') {
       return value.length > 30 ? <TextArea _key={_key} value={value} /> : <Input _key={_key} value={value} />
+    } else if (typeof value == 'boolean') {
+      return <Checkbox _key={_key} value={value} />
     } else {
       return (
         <span>
@@ -50,31 +46,6 @@ const Form = ({ data }: any) => {
         </span>
       )
     }
-
-    // if (typeof value === 'string') {
-    //   if (value.length > 30) {
-    //     return <TextArea _key={_key} value={value} />
-    //   } else {
-    //     return <Input _key={_key} value={value} />
-    //   }
-    // } else {
-    //   return (
-    //     <span>
-    //       <h4>{_key}:</h4>
-    //       <input defaultValue={value.toString() || ''} />
-    //     </span>
-    //   )
-    // }
-
-    // switch (type) {
-    //   case 'string':
-    //   case 'longstring':
-    //     return <TextArea _key={_key} value={value} />
-    //   case 'date':
-    //     return <Datepicker _key={_key} value={value} />
-    //   case 'boolean':
-    //     return <Checkbox _key={_key} value={value} />
-    //   default:
   }
 
   return (
@@ -84,7 +55,7 @@ const Form = ({ data }: any) => {
           return <DynamicInput _key={_key} value={data[_key]} />
         })
       ) : (
-        <NoCookies>no data </NoCookies>
+        <NoCookies>no data</NoCookies>
       )}
     </>
   )
