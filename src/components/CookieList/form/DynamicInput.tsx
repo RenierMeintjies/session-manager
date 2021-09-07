783e1219eeeb399d33d08e844cd9e1cbc14c4ee1
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2021-09-07 21:52:34 +0200

    fix formating

--- src/components/CookieList/form/DynamicInput.tsx
+++ src/components/CookieList/form/DynamicInput.tsx
@@ -1,5 +1,5 @@
-import { inputData } from '../../../interfaces';
-import { FormItemContainer } from './style';
+import { inputData } from '../../../interfaces'
+import { FormItemContainer } from './style'
 
 const Input = ({ _key, value }: inputData) => {
   return (
@@ -7,8 +7,8 @@ const Input = ({ _key, value }: inputData) => {
       <h4>{_key}:</h4>
       <input defaultValue={value.toString() || ''} />
     </FormItemContainer>
-  );
-};
+  )
+}
 
 const TextArea = ({ _key, value }: inputData) => {
   return (
@@ -16,8 +16,8 @@ const TextArea = ({ _key, value }: inputData) => {
       <h4>{_key}:</h4>
       <textarea defaultValue={value.toString() || ''} />
     </FormItemContainer>
-  );
-};
+  )
+}
 
 const Checkbox = ({ _key, value }: inputData) => {
   return (
@@ -25,22 +25,22 @@ const Checkbox = ({ _key, value }: inputData) => {
       <h4>{_key}:</h4>
       <input type="checkbox" defaultValue={value.toString() || ''} />
     </FormItemContainer>
-  );
-};
+  )
+}
 
 const DynamicInput = ({ _key, value }: inputData) => {
   if (typeof value === 'string') {
-    return value.length > 30 ? <TextArea _key={_key} value={value} /> : <Input _key={_key} value={value} />;
+    return value.length > 30 ? <TextArea _key={_key} value={value} /> : <Input _key={_key} value={value} />
   } else if (typeof value == 'boolean') {
-    return <Checkbox _key={_key} value={value} />;
+    return <Checkbox _key={_key} value={value} />
   } else {
     return (
       <FormItemContainer>
         <h4>{_key}:</h4>
         <input defaultValue={value.toString() || ''} />
       </FormItemContainer>
-    );
+    )
   }
-};
+}
 
-export default DynamicInput;
+export default DynamicInput
