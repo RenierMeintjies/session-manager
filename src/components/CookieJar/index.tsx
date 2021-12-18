828f7585ffdd4fe0ec21ff433bf5a614eb0d2ba8
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2021-12-18 20:27:35 +0200

    WIP: start of localstorage logic

--- src/components/CookieJar/index.tsx
+++ src/components/CookieJar/index.tsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useCookies } from '../../hooks/cookies'
 import * as S from './style'
+
+const useStateWithLocalStorage = (localStorageKey: string) => {
+  const [value, setValue] = useState(localStorage.getItem(localStorageKey) || '')
+  const cookies = useCookies(false)
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(cookies))
+  }, [value])
+
+  return [value, setValue]
+}
 
 const CookieJar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [value] = useStateWithLocalStorage('myValueInLocalStorage')
+
+  const myStorage = sessionStorage.setItem('sessionStorageKey', value.toString())
 
   const toggling = () => setIsOpen(!isOpen)
 
@@ -13,7 +28,7 @@ const CookieJar = () => {
         {isOpen && (
           <S.DropDownListContainer>
             <S.DropDownList>
-              <S.ListItem>Facebook</S.ListItem>
+              <S.ListItem>Storage{JSON.stringify(myStorage)}</S.ListItem>
               <S.ListItem>Youtube</S.ListItem>
               <S.ListItem>Github</S.ListItem>
             </S.DropDownList>
