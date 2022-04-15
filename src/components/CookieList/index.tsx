3b8d06b2f4a88f7dd71383eb62d2de456766126a
Cullen Botha <culbot@gmail.com>
2022-04-15 23:01:23 +0200

    group cookies per domain, hide by default, WIP #16

--- src/components/CookieList/index.tsx
+++ src/components/CookieList/index.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import CookieRow from './CookieRow'
-import chromeApi from '../../api/chrome'
-
-import { useCookies } from '../../hooks/useCookies'
 import * as S from './style'
+import chromeApi from '../../api/chrome'
+import CookieRow from './CookieRow'
 import { Delete as DeleteIcon } from '@material-ui/icons'
+import { groupBy } from 'lodash'
+import { useCookies } from '../../hooks/useCookies'
+import { useState } from 'react'
 
 const CookieList = () => {
   const [seeAllDomains, setSeeAllDomains] = useState(false)
@@ -22,22 +22,52 @@ const CookieList = () => {
     setSeeAllDomains(!seeAllDomains)
   }
 
-  return (
-    <S.Container>
-      <S.Cell>Key</S.Cell>
-      <S.Cell>Value</S.Cell>
-      <S.Cell>
-        {cookies.length > 0 && <DeleteIcon onClick={handleDeleteAll} />}
-        <button onClick={handleToggleClick}>{seeAllDomains ? 'current domain' : 'see all domains'}</button>
-      </S.Cell>
-      {cookies.length > 0 ? (
-        cookies.map((cookie: any, index: number) => {
+  const RenderCookieRows = ({ cookies }: any) => {
+    return (
+      <S.Container domainView={seeAllDomains}>
+        <S.Cell>Key</S.Cell>
+        <S.Cell>Value</S.Cell>
+        <S.Cell>
+          {cookies.length > 0 && <DeleteIcon onClick={handleDeleteAll} />}
+          <button onClick={handleToggleClick}>{seeAllDomains ? 'current domain' : 'see all domains'}</button>
+        </S.Cell>
+
+        {cookies.map((cookie: any, index: number) => {
           return <CookieRow key={`cookie-${index}-${cookie.domain}`} cookie={cookie} index={index} />
-        })
+        })}
+      </S.Container>
+    )
+  }
+
+  const RenderDomainRows = () => {
+    const groupedCookies = groupBy(cookies, 'domain')
+
+    return (
+      <>
+        {Object.keys(groupedCookies).map((domain: string, index: number) => {
+          return (
+            <S.Details>
+              <S.Summary>{domain}</S.Summary>
+              <RenderCookieRows cookies={groupedCookies[domain]} />
+            </S.Details>
+          )
+        })}
+      </>
+    )
+  }
+
+  return (
+    <>
+      {cookies.length > 0 ? (
+        seeAllDomains ? (
+          <RenderDomainRows />
+        ) : (
+          <RenderCookieRows cookies={cookies} />
+        )
       ) : (
         <S.NoCookies>No cookies for you :c</S.NoCookies>
       )}
-    </S.Container>
+    </>
   )
 }
 
