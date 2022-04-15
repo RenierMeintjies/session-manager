7d5b32e4cfa42bf9d2685d33f0585d98b519552e
Cullen Botha <culbot@gmail.com>
2022-04-15 21:45:13 +0200

    more seed data

--- src/api/chrome/mock-seed.js
+++ src/api/chrome/mock-seed.js
@@ -3,14 +3,16 @@ module.exports = () => {
   const data = { cookies: [] }
   for (let i = 1; i < 12; i++) {
     const isLoggedInSeed = Math.floor(Math.random() * 11)
-    data.cookies.push({
-      id: i,
-      name: `cookiethisisatestignorefornowpleasethanks-${i}`,
-      domain: `domain-${i}.com`,
-      value: Math.random().toString(36).substring(2),
-      date: new Date(),
-      isLoggedIn: isLoggedInSeed > 5 ? true : false,
-    })
+    for (let i = 1; i < 12; i++) {
+      data.cookies.push({
+        id: i,
+        name: `cookiethisisatestignorefornowpleasethanks-${i}`,
+        domain: `domain-${i}.com`,
+        value: Math.random().toString(36).substring(2),
+        date: new Date(),
+        isLoggedIn: isLoggedInSeed > 5 ? true : false,
+      })
+    }
   }
   return data
 }
